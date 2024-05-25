08af60542dec2ef27077bab7c190333bea8a2264
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-25 12:09:57 +0800

    add getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
   let x = Math.random();
   if (x < 1 / 3) {
-    return "rock";
+    return CHOICES[0];
   } else if (x < 2 / 3) {
-    return "paper";
+    return CHOICES[1];
   }
-  return "scissors";
+  return CHOICES[2];
+}
+
+function getHumanChoice() {
+  let humanChoice;
+  do {
+    humanChoice = prompt(`Enter ${CHOICES[0]}, ${CHOICES[1]}, or ${CHOICES[2]}:`);
+  } while (!CHOICES.includes(humanChoice));
+  return humanChoice;
 }
