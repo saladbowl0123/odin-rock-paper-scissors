19ccfc399d0717811d2831b917bddc913cf219a9
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-25 13:26:36 +0800

    add playGame

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
 const CHOICES = ["rock", "paper", "scissors"];
 const HUMAN_CHOICE_PROMPT = `Enter ${CHOICES[0]}, ${CHOICES[1]}, or ${CHOICES[2]}:`;
-const DRAW = `Draw`;
-const WIN = `Win`;
-const LOSE = `Lose`;
-const HUMAN_SCORE = `Human score`;
-const COMPUTER_SCORE = `Computer score`;
-
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
   let x = Math.random();
@@ -27,23 +19,54 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  roundStatus = (CHOICES.indexOf(humanChoice)
-    - CHOICES.indexOf(computerChoice)) % 3;
-  switch (roundStatus) {
-    case 0:
-      console.log(`${DRAW}: ${humanChoice}`);
-      break;
-    case 1:
-      console.log(`${WIN}: ${humanChoice} beats ${computerChoice}`);
-      ++humanScore;
-      break;
-    default:
-      console.log(`${LOSE}: ${computerChoice} beats ${humanChoice}`);
-      ++computerScore;
+function playGame(numRounds) {
+
+  const ROUND = `Round`;
+  const DRAW = `Draw`;
+  const WIN = `Win`;
+  const LOSE = `Lose`;
+  const HUMAN_SCORE = `Human score`;
+  const COMPUTER_SCORE = `Computer score`;
+  const HUMAN_WIN_GAME = `Human wins the game`;
+  const COMPUTER_WIN_GAME = `Computer wins the game`;
+  const DRAW_GAME = `Game draw`;
+
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(roundNo, humanChoice, computerChoice) {
+    console.log(`${ROUND} ${roundNo}`);
+    roundStatus = (CHOICES.indexOf(humanChoice)
+      - CHOICES.indexOf(computerChoice)) % 3;
+    switch (roundStatus) {
+      case 0:
+        console.log(`${DRAW}: ${humanChoice}`);
+        break;
+      case 1:
+        console.log(`${WIN}: ${humanChoice} beats ${computerChoice}`);
+        ++humanScore;
+        break;
+      default:
+        console.log(`${LOSE}: ${computerChoice} beats ${humanChoice}`);
+        ++computerScore;
+    }
+    console.log(
+      `${HUMAN_SCORE}: ${humanScore}\n${COMPUTER_SCORE}: ${computerScore}`);
   }
-  console.log(
-    `${HUMAN_SCORE}: ${humanScore}\n${COMPUTER_SCORE}: ${computerScore}`);
+
+  for (let i = 0; i < numRounds; i++) {
+    playRound(i + 1, getHumanChoice(), getComputerChoice());
+  }
+
+  if (humanScore > computerScore) {
+    console.log(HUMAN_WIN_GAME);
+  } else if (humanScore < computerScore) {
+    console.log(COMPUTER_WIN_GAME);
+  } else {
+    console.log(DRAW_GAME);
+  }
 }
 
-while (true) { playRound(getHumanChoice(), getComputerChoice()); }
+let numRounds = 5;
+
+playGame(numRounds);
