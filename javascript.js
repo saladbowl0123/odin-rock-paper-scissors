10c4a9d81dc8dc09da959778ed7fdd26bfa2c1d8
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-25 11:50:52 +0800

    add getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+function getComputerChoice() {
+  let x = Math.random();
+  if (x < 1 / 3) {
+    return "rock";
+  } else if (x < 2 / 3) {
+    return "paper";
+  }
+  return "scissors";
+}
